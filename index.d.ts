91d81d12e7775de50e062e5b65f791a3f7f6d262
Zico Deng <zicodeng@gmail.com>
2020-04-16 18:55:39 -0700

    Fix typing

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,20 @@
-export enum ClimateType {
-  MarineWestcoastClimate = 'Marine Westcoast Climate',
-  MediterraneanClimate = 'Mediterranean Climate',
-  HumidContinentalClimate = 'Humid Continental Climate',
-  HumidSubtropicalClimate = 'Humid Subtropical Climate',
-  SemiaridSteppeClimate = 'Semiarid Steppe Climate',
-  HighlandClimate = 'Highland Climate',
-  MidlatitudeDesertClimate = 'Midlatitude Desert Climate',
-}
+export type ClimateType =
+  | 'Marine Westcoast Climate'
+  | 'Mediterranean Climate'
+  | 'Humid Continental Climate'
+  | 'Humid Subtropical Climate'
+  | 'Semiarid Steppe Climate'
+  | 'Highland Climate'
+  | 'Midlatitude Desert Climate';
 
-export enum ClimateTypeTranslation {
-  MarineWestcoastClimate = '温带海洋性气候',
-  MediterraneanClimate = '地中海气候',
-  HumidContinentalClimate = '湿润大陆性气候',
-  HumidSubtropicalClimate = '亚热带湿润气候',
-  SemiaridSteppeClimate = '半干旱草原气候',
-  HighlandClimate = '高原山地气候',
-  MidlatitudeDesertClimate = '中纬度沙漠气候',
-}
+export type ClimateTypeTranslation =
+  | '温带海洋性气候'
+  | '地中海气候'
+  | '湿润大陆性气候'
+  | '亚热带湿润气候'
+  | '半干旱草原气候'
+  | '高原山地气候'
+  | '中纬度沙漠气候';
 
 export interface USState {
   name: string;
